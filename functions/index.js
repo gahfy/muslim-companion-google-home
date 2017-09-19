985939cf3c9ee12d4d89d9409d9688609ae4f5b4
Gaetan HERFRAY <gaetan.hfy@gmail.com>
2017-09-19 23:03:28 +0200

    first final version

--- functions/index.js
+++ functions/index.js
@@ -9,65 +9,79 @@ exports.salattimes = functions.https.onRequest((req, res) => {
     const app = new ActionsSdkApp({request: req, response: res});
 
     const mainIntent = function(app){
-        let coordinates = {latitude: 48.856638, longitude: 2.352241}
-        let country = wc([coordinates.longitude, coordinates.latitude]);
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth()+1; //January is 0!
-        let yyyy = today.getFullYear();
+        let preciseLocationPermission = app.SupportedPermissions.DEVICE_PRECISE_LOCATION
+
+        // Ask for one permission
+        app.askForPermission('To calculate salat times', preciseLocationPermission);
+    }
+
+    const permissionIntent = function(app){
+        if (app.isPermissionGranted()) {
+            let coordinates = app.getDeviceLocation().coordinates;
+            let country = wc([coordinates.longitude, coordinates.latitude]);
+            let today = new Date();
+            let dd = today.getDate();
+            let mm = today.getMonth()+1; //January is 0!
+            let yyyy = today.getFullYear();
 
 
-        let ptu = new PrayerTimeUtils(yyyy, mm, dd, coordinates.latitude, coordinates.longitude, 0, 0);
-        ptu.changeCountry(country);
+            let ptu = new PrayerTimeUtils(yyyy, mm, dd, coordinates.latitude, coordinates.longitude, 0, 0);
+            ptu.changeCountry(country);
 
-        timestampFajr = ptu.getFajrTimestamp();
-        timestampSunrise = ptu.getSunriseTimestamp();
-        timestampDhuhr = ptu.getDhuhrTimestamp();
-        timestampAsr = ptu.getAsrTimestamp();
-        timestampMaghrib = ptu.getMaghribTimestamp();
-        timestampIsha = ptu.getIshaTimestamp();
+            timestampFajr = ptu.getFajrTimestamp();
+            timestampSunrise = ptu.getSunriseTimestamp();
+            timestampDhuhr = ptu.getDhuhrTimestamp();
+            timestampAsr = ptu.getAsrTimestamp();
+            timestampMaghrib = ptu.getMaghribTimestamp();
+            timestampIsha = ptu.getIshaTimestamp();
 
-        timezoner.getTimeZone(
-    		coordinates.latitude, // Latitude coordinate
-    		coordinates.longitude, // Longitude coordinate
-    		function (err, data) {
-                let timezone = null;
-    			if (err) {
-    				timezone = null;
-    			} else {
-    				timezone = data.timeZoneId;
-    			}
-                fajrTime = moment(timestampFajr).format('hh:mma');
-                sunriseTime = moment(timestampSunrise).format('hh:mma');
-                dhuhrTime = moment(timestampDhuhr).format('hh:mma');
-                asrTime = moment(timestampAsr).format('hh:mma');
-                maghribTime = moment(timestampMaghrib).format('hh:mma');
-                ishaTime = moment(timestampIsha).format('hh:mma');
+            timezoner.getTimeZone(
+        		coordinates.latitude, // Latitude coordinate
+        		coordinates.longitude, // Longitude coordinate
+        		function (err, data) {
+                    let timezone = null;
+        			if (err) {
+        				timezone = null;
+        			} else {
+        				timezone = data.timeZoneId;
+        			}
+                    fajrTime = moment(timestampFajr).format('hh:mma');
+                    sunriseTime = moment(timestampSunrise).format('hh:mma');
+                    dhuhrTime = moment(timestampDhuhr).format('hh:mma');
+                    asrTime = moment(timestampAsr).format('hh:mma');
+                    maghribTime = moment(timestampMaghrib).format('hh:mma');
+                    ishaTime = moment(timestampIsha).format('hh:mma');
 
-                if(timezone != null){
-                    fajrTime = moment(timestampFajr).tz(timezone).format('hh:mma');
-                    sunriseTime = moment(timestampSunrise).tz(timezone).format('hh:mma');
-                    dhuhrTime = moment(timestampDhuhr).tz(timezone).format('hh:mma');
-                    asrTime = moment(timestampAsr).tz(timezone).format('hh:mma');
-                    maghribTime = moment(timestampMaghrib).tz(timezone).format('hh:mma');
-                    ishaTime = moment(timestampIsha).tz(timezone).format('hh:mma');
-                }
+                    if(timezone != null){
+                        fajrTime = moment(timestampFajr).tz(timezone).format('hh:mma');
+                        sunriseTime = moment(timestampSunrise).tz(timezone).format('hh:mma');
+                        dhuhrTime = moment(timestampDhuhr).tz(timezone).format('hh:mma');
+                        asrTime = moment(timestampAsr).tz(timezone).format('hh:mma');
+                        maghribTime = moment(timestampMaghrib).tz(timezone).format('hh:mma');
+                        ishaTime = moment(timestampIsha).tz(timezone).format('hh:mma');
+                    }
 
-                let speech = "<speak>"+
-                "<s>Today, fajr is at <say-as interpret-as=\"time\" format=\"hms12\">"+fajrTime+"</say-as></s>"+
-                "<s>Sunrise is at <say-as interpret-as=\"time\" format=\"hms12\">"+sunriseTime+"</say-as></s>"+
-                "<s>Dhuhr is at <say-as interpret-as=\"time\" format=\"hms12\">"+dhuhrTime+"</say-as></s>"+
-                "<s>Asr is at <say-as interpret-as=\"time\" format=\"hms12\">"+asrTime+"</say-as></s>"+
-                "<s>Maghrib is at <say-as interpret-as=\"time\" format=\"hms12\">"+maghribTime+"</say-as></s>"+
-                "<s>Isha is at <say-as interpret-as=\"time\" format=\"hms12\">"+ishaTime+"</say-as></s>"+
-                "</speak>";
+                    let speech = "<speak>"+
+                    "<s>Today, fajr is at <say-as interpret-as=\"time\" format=\"hms12\">"+fajrTime+"</say-as></s>"+
+                    "<s>Sunrise is at <say-as interpret-as=\"time\" format=\"hms12\">"+sunriseTime+"</say-as></s>"+
+                    "<s>Dhuhr is at <say-as interpret-as=\"time\" format=\"hms12\">"+dhuhrTime+"</say-as></s>"+
+                    "<s>Asr is at <say-as interpret-as=\"time\" format=\"hms12\">"+asrTime+"</say-as></s>"+
+                    "<s>Maghrib is at <say-as interpret-as=\"time\" format=\"hms12\">"+maghribTime+"</say-as></s>"+
+                    "<s>Isha is at <say-as interpret-as=\"time\" format=\"hms12\">"+ishaTime+"</say-as></s>"+
+                    "</speak>";
 
-                app.tell(speech);
-    		}
-    	);
+                    app.tell(speech);
+        		}
+        	);
+        }
+        else{
+            app.tell("Sorry, I can not help you without knowing your position.");
+        }
     }
 
     let actionMap = new Map();
     actionMap.set(app.StandardIntents.MAIN, mainIntent);
+    actionMap.set(app.StandardIntents.PERMISSION, permissionIntent);
+
     app.handleRequest(actionMap);
 });
